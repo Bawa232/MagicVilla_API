daf7c9001e461cf8ed902ff3905f768e2f168612
JBawa <JBawa@HOBO-IT-LT25>
2024-06-03 09:36:17 +0100

    made changes

---
+++ MagicVilla__VillaApi/Data/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla__VillaApi.Models.Dto;
+
+namespace MagicVilla__VillaApi.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+            {
+
+                new VillaDTO{Id = 1, Name = "Pool View", Sqft = 300, Occupancy=12 },
+                new VillaDTO{Id = 2, Name = "Beach View", Sqft = 600, Occupancy=2 }
+            };
+    }
+}
---
+++ MagicVilla__VillaApi/Models/villa.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla__VillaApi.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
